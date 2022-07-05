3f94b020d3481c740a93704368b07ff687c929e3
Bogdan Bylinski <bylinski.bogdan@gmail.com>
2022-07-06 00:02:30 +0300

    gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,11 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CityWeather from "./components/CityWeather";
 import Home from "./components/Home";
+import Main from "./Main";
 
 function App() {
   return (
-    <>
-    <BrowserRouter>
-    <Home></Home>
-      <Routes>
-      <Route index element={<Home ></Home>}></Route>
-      <Route path="/" element={<Home ></Home>}></Route>
-      <Route path="/home" element={<Home ></Home>}></Route>
-      <Route path="/city/:name" element={<CityWeather></CityWeather>}></Route>
-
-      </Routes>
-    </BrowserRouter>
-    </>
+    <Main></Main>
   );
 }
 
---
+++ src/Main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CityWeather from "./components/CityWeather";
+import Home from "./components/Home";
+
+function Main() {
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+      <Route index element={<Home ></Home>}></Route>
+      <Route path="/" element={<Home ></Home>}></Route>
+      <Route path="/home" element={<Home ></Home>}></Route>
+      <Route path="/city/:name" element={<CityWeather></CityWeather>}></Route>
+
+      </Routes>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default Main;
