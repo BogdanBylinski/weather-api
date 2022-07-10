f2db64b35f76836acf1785b1e037204082c85eb1
Bogdan Bylinski <bylinski.bogdan@gmail.com>
2022-07-10 11:53:59 +0300

    temp bar background gradients

--- src/components/Days.jsx
+++ src/components/Days.jsx
@@ -9,10 +9,6 @@ function Days({ city, today }) {
   const [day, setDay]=useState('')
 //   const [dayNow, setDayNow]=useState('')
   let dayNow = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][new Date().getDay()]
-  console.log(city);
-  console.log(today);
-  console.log(minTemp);
-  console.log(maxTemp);
   
   const getDayName = (forecastday) =>{
     let date = new Date(forecastday);
@@ -34,8 +30,6 @@ useEffect(()=>{
       today.forecast.forecastday.map(i => (temp.push(Math.floor(i.day.maxtemp_c))))
       today.forecast.forecastday.map(i => (temp2.push(Math.floor(i.day.mintemp_c))))
       
-      console.log(Math.max(...temp), 'hid');
-      console.log(Math.min(...temp2), 'hidd');
       setMaxTemp(Math.max(...temp));
       setMinTemp(Math.min(...temp2))
   
@@ -44,9 +38,6 @@ useEffect(()=>{
     }
   },[city])
   const left = () => {
-      console.log(Math.floor(city.day.mintemp_c) - minTemp );
-      console.log(maxTemp);
-      console.log(minTemp);
       if (minTemp &&
           
           (Math.floor(city.day.mintemp_c) - minTemp) !== 0
@@ -54,7 +45,7 @@ useEffect(()=>{
         let range = (maxTemp - minTemp) / 10;
       let width =
         (Math.floor(city.day.mintemp_c) - minTemp) / range 
-       return (Math.ceil(width));
+       return (Math.ceil(width)*2);
     } else if (
       
       Math.floor(city.day.mintemp_c) - minTemp === 0 
@@ -67,31 +58,76 @@ useEffect(()=>{
      return (width);
   };
   const right = () => {
-
-    let range = (maxTemp - minTemp) / 10;
-    console.log(range);
-    console.log(maxTemp);
-    if (
-      
-      maxTemp - Math.floor(city.day.maxtemp_c) !== 0
-    ) {
+    let result =0
+      if (maxTemp - Math.floor(city.day.maxtemp_c) > 0) {
+        let range = (maxTemp - minTemp) / 10;
       let width = maxTemp - Math.floor(city.day.maxtemp_c) / range;
-       return (Math.floor(width));
+       result= Math.floor(width);
     } else if (
       
-      maxTemp - Math.floor(city.day.maxtemp_c) === 0
+      maxTemp - Math.floor(city.day.maxtemp_c) <= 0
     ) {
-       return (0);
-    }
+       result = 0;
+    }   
+    return result*2
   };
 
   const position = () => {
    
       let range =
         ((today.current.temp_c - minTemp) / (maxTemp - minTemp / 10)) * 100;
+       if(range<0){return 3
+    }   
+       else if(today.current.temp_c>maxTemp){return 96.5
+
+    }   else
        return(Math.floor(range));
     
   };
+  const color1 = ()=>{
+      let color1 = '';
+    //   let color2 = '';
+      if(city.day.mintemp_c<=10 && city.day.mintemp_c >=0) {
+           color1 = '#90ee90'
+        }
+        else if(city.day.mintemp_c<-10 && city.day.mintemp_c <0){
+             color1 ='#3399FF'
+        }
+        else if(city.day.mintemp_c>10 && city.day.mintemp_c <=20){
+             color1 ='#ffff00'
+        }
+        else if(city.day.mintemp_c>20 && city.day.mintemp_c <=30){
+             color1 ='#FFA500'
+        } 
+       
+        else if(city.day.mintemp_c>30 ){
+             color1 ='#FF0000'
+            //   color2 = '#990 099'
+        }
+      return color1
+  }
+  const color2 =()=>{
+      let color2 =''
+     if(city.day.maxtemp_c<-10 && city.day.maxtemp_c <0){
+        color2 ='#90ee90'
+   }
+   else if(city.day.maxtemp_c<=0 && city.day.maxtemp_c <10){
+        color2 ='#3399FF'
+   }
+   else if(city.day.maxtemp_c>=10 && city.day.maxtemp_c <20){
+        color2 ='#ffff00'
+   }
+   else if(city.day.maxtemp_c>=20 && city.day.maxtemp_c <30){
+        color2 ='#FFA500'
+   }
+   else if(city.day.maxtemp_c>30 ){
+    color2 ='#FF0000'
+   }
+   return color2
+  }
+  const gradient =()=>{
+      return  `linear-gradient(90deg, ${color1()} 0%, ${color2()} 100%)`
+  }
   return city ? <>
   <div className="oneCity__forecast-body-item">
   <div className="oneCity__forecast-body-item-day">
@@ -99,7 +135,7 @@ useEffect(()=>{
   <div className="oneCity__forecast-body-item-clouds">
     <img src={city.day.condition.icon} alt="icon" />
    <span>
-     {city.day.daily_will_it_rain === 1? `${city.day.daily_chance_of_rain}%` : ''
+     {city.day.daily_will_it_rain === 1? `${city.day.daily_chance_of_rain}%` : '' 
 }
      </span> 
   </div>
@@ -108,7 +144,7 @@ useEffect(()=>{
     <div className="mainLine">
 
 <div className="leftLine" style={{width: `${left()}%`}}></div>
-<div className="midLine" style={{width: `${middle()}%`}}></div>
+<div className="midLine"  style={{width: `${middle()}%`, background: `linear-gradient(90deg, ${color1()} 0%, ${color2()} 100%)`}}></div>
 <div className="rightLine" style={{width: `${right()}%`}}></div>
 {
 
